f0efe9cee1e55980d5d189f225a3849e44ab22a6
Andreas Unterkircher <unki@netshadow.net>
2015-11-29 07:28:15 +0100

    add functions.sh

---
+++ functions.sh
@@ -0,0 +1,197 @@
+#
+# found at https://raymii.org/s/snippets/Bash_Bits_Check_If_Item_Is_In_Array.html
+#
+
+# Colors
+readonly COLOR_RESTORE='\e[0m'
+readonly COLOR_GREEN='\e[0;32m'
+readonly COLOR_RED='\e[0;31m'
+
+in_array () {
+
+    local -a 'haystack=("${'"$1"'[@]}")'
+    local needle="${2}"
+    for i in "${haystack[@]}"; do
+        if [[ "${i}" =~ "${needle}" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+log_begin_msg () {
+   [ "x${DEBUG}" == "xtrue" ] || exit 0
+   echo -n "$1"
+}
+
+log_end_msg () {
+
+   [ "x${DEBUG}" == "xtrue" ] || exit 0
+
+   if [ -z $! ]; then
+      echo " done"
+      return;
+   fi
+
+   if [[ $1 =~ ^[0-9]+$ ]]; then
+      if [ $1 -eq 0 ]; then
+         log_msg "done"
+      elif [ $1 -eq 1 ]; then
+         log_failure_msg "failed"
+      else
+         log_failure_msg " unknown (${1})"
+      fi
+      return
+   fi
+
+   echo " $1"
+}
+
+log_msg () {
+   [ "x${DEBUG}" == "xtrue" ] || exit 0
+   echo "$1"
+}
+
+log_success_msg () {
+   echo -n ' '
+   echo -en ${COLOR_GREEN}
+   echo -n "${1}"
+   echo -e ${COLOR_RESTORE}
+}
+
+log_failure_msg () {
+   #[ "x${DEBUG}" == "xtrue" ] || exit 0
+   echo -n ' '
+   echo -en ${COLOR_RED}
+   echo -n " ${1}"
+   echo -e ${COLOR_RESTORE}
+}
+
+check_privileges () {
+   if [[ ( -z "${LOGNAME}" || "${LOGNAME}" != "root" ) && \
+      ( -z "${USERNAME}" || "${USERNAME}" != "root" ) ]]; then
+
+      log_failure_msg "`basename $0` must be invoked with root privileges!"
+      exit 1
+   fi
+}
+
+check_requirements () {
+   if [ -z "${BASH_VERSION}" ]; then
+      echo "\$BASH_VERSION variable is not set, is this really a Bash shell?"
+      exit 1
+   fi
+   if [ -z "${BASH_VERSINFO[0]}" ]; then
+      echo "\$BASH_VERSINFO variable is not set, is this really a Bash shell?"
+      exit 1
+   fi
+   if [ ${BASH_VERSINFO[0]} -lt 4 ] || \
+      ( [ ${BASH_VERSINFO[0]} == 4 ] && [ ${BASH_VERSINFO[1]} -lt 2 ] ); then
+      echo "Bash >= v4.2 is required!"
+      exit 1
+   fi
+}
+
+check_configuration () {
+   #
+   # vtysh
+   #
+   if [ -z "${VTYSH}" ]; then
+      log_failure_msg "\$VTYSH is not set!"
+      exit 1
+   fi
+   if [ ! -e "${VTYSH}" ]; then
+      log_failure_msg "vtysh not found at ${VTYSH}!"
+      exit 1
+   fi
+   if [ ! -x "${VTYSH}" ]; then
+      log_failure_msg "${VTYSH} is not executable!"
+      exit 1
+   fi
+
+   #
+   # quagga configuration directory.
+   #
+   if [ -z "${QUAGGA_CONF_DIR}" ]; then
+      log_failure_msg "\$QUAGGA_CONF_DIR is not set!"
+      exit 1
+   fi
+   if [ ! -d "${QUAGGA_CONF_DIR}" ]; then
+      log_failure_msg "${QUAGGA_CONF_DIR} is not a directory!"
+      exit 1
+   fi
+   if [ ! -x "${QUAGGA_CONF_DIR}" ]; then
+      log_failure_msg "No permission to enter directory ${QUAGGA_CONF_DIR}!"
+      exit 1
+   fi
+
+   #
+   # prestaging directory.
+   #
+   if [ -z "${PRESTAGE_DIR}" ]; then
+      log_failure_msg "\$PRESTAGE_DIR is not set!"
+      exit 1
+   fi
+   if [ ! -d "${PRESTAGE_DIR}" ]; then
+      log_failure_msg "${PRESTAGE_DIR} is not a directory!"
+      exit 1
+   fi
+   if [ ! -x "${PRESTAGE_DIR}" ]; then
+      log_failure_msg "No permission to enter directory ${PRESTAGE_DIR}!"
+      exit 1
+   fi
+}
+
+check_parameters () {
+   if [ $# -lt 1 ] || [ -z $1 ]; then
+      show_help;
+   fi
+
+   while getopts :hd:v OPTS; do
+      ARGSPARSED=1
+      case $OPTS in
+         h)
+            show_help
+            exit 0
+            ;;
+         d)
+            readonly DAEMON=${OPTARG,,}
+            ;;
+         v)
+            readonly DEBUG=true
+            ;;
+         *)
+            log_failure_msg "Invalid parameter(s)!"
+            echo
+            show_help
+            exit 1
+            ;;
+      esac
+   done
+
+   if [ -z "${ARGSPARSED}" ] || [ -z "${DAEMON}" ]; then
+      log_failure_msg "Invalid parameter(s)!"
+      echo
+      show_help
+      exit 1
+   fi
+
+   if [ "x${DAEMON}" != "xospfd" ] && \
+      [ "x${DAEMON}" != "xbgpd" ] && \
+      [ "x${DAEMON}" != "xzebra" ]; then
+      log_failure_msg "${1} is not supported!"
+      exit 1
+   fi
+}
+
+show_help ()
+{
+echo $0
+echo
+echo "  -h     ... help"
+echo "  -n arg ... node"
+echo "  -o arg ... object"
+echo "  -k arg ... key"
+echo "  -f arg ... write to file"
+echo
+}
